0157b2f055b2b466e7dfa58506145dc38ab569dd
Kalisvaart <j.e.b.kalisvaart@student.tue.nl>
2018-11-26 15:05:53 +0100

    trying out code

--- Code/Storage of dataset.py
+++ Code/Storage of dataset.py
@@ -5,7 +5,7 @@ def read_data(filename):
     
     df = pd.read_csv(filename, sep = ' ', index_col = False)
     
-    data = np.zeros((1232, 981, 983), np.int32)  
+    data = np.zeros((1232, 981, 983), np.int32)
     
     for row in df.iterrows():
         time = row[1][0]
@@ -17,5 +17,8 @@ def read_data(filename):
     
     return data
 
-data = read_data('./profile_semantic_trafo_final.txt')
+data = read_data('profile_semantic_trafo_final.txt')
 
+np.sum(data[64])
+test = pd.read_csv('profile_semantic_trafo_final.txt', sep = ' ', index_col = False)
+sum(test[test["time"]==64]["weight"])
