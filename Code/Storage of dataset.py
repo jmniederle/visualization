193bcd5cd86e9cc2302dec12debbf28b9680a5d8
Kalisvaart <j.e.b.kalisvaart@student.tue.nl>
2018-11-26 15:20:13 +0100

    Update Storage of dataset.py

--- Code/Storage of dataset.py
+++ Code/Storage of dataset.py
@@ -5,7 +5,7 @@ def read_data(filename):
 
     df = pd.read_csv(filename, sep = ' ', index_col = False)
 
-    data = np.zeros((1232, 981, 983), np.int64)
+    data = np.zeros((1232, 981, 983), np.float)
 
     for row in df.iterrows():
         time = row[1][0]
@@ -22,7 +22,7 @@ data = read_data('profile_semantic_trafo_final.txt')
 #def aggregate_time(start, end)
 type(data[1])
 np.sum(data[1])
-data[64][:][1]
+    data[1][:][1]
 
 test = pd.read_csv('profile_semantic_trafo_final.txt', sep = ' ', index_col = False)
 weights = test[test["time"]==1]["weight"]
