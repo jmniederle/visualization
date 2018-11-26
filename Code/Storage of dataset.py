dfad3b29fe7762f5a19e12d052696be622a80f35
jmniederle <j.m.niederle@student.tue.nl>
2018-11-26 15:06:50 +0100

    test
    tset

--- Code/Storage of dataset.py
+++ Code/Storage of dataset.py
@@ -5,7 +5,7 @@ def read_data(filename):
     
     df = pd.read_csv(filename, sep = ' ', index_col = False)
     
-    data = np.zeros((1232, 981, 983), np.int32)  
+    data = np.zeros((1232, 981, 983), np.int64)  
     
     for row in df.iterrows():
         time = row[1][0]
@@ -17,5 +17,19 @@ def read_data(filename):
     
     return data
 
-data = read_data('./profile_semantic_trafo_final.txt')
+data = read_data('profile_semantic_trafo_final.txt')
 
+#def aggregate_time(start, end)
+type(data[1])
+np.sum(data[1])
+data[64][:][1]
+
+test = pd.read_csv('profile_semantic_trafo_final.txt', sep = ' ', index_col = False)
+weights = test[test["time"]==1]["weight"]
+sum(weights)
+
+testsum = 0
+for i in range(1,981):
+    testsum += sum(data[1][i])
+    
+testsum
