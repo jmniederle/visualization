ff014d0b40f2c0bb4b1d5701b862eca6bab1a643
Thomasq99 <t.j.quadt@student.tue.nl>
2018-11-30 12:43:47 +0100

    Update FinalApp.py

--- Code/FinalApp.py
+++ Code/FinalApp.py
@@ -15,25 +15,32 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.scripts.config.serve_locally = True
 
+markdown_text= '''
+Welcome to our Visualization tool!
+a
 
-app.layout = html.Div([ 
-        
-            html.Div([html.H1('Visualization Group 8', style={'textAlign': 'center', 'borderStyle': 'dashed'})]),
-        
-            html.Div(
-                className="row",
-                children=[               
-            # Start Column 1
-            html.Div(className= "six columns",
-                     children=[
-                    html.H2('Hallo')], 
-                    style= {'width':'100%', 'borderStyle': 'dashed'}), 
-        
-            
-            # Start middle column
-            html.Div(
-                    className= "six-columns",
-                    children=[
+a
+a
+a
+a
+a
+a
+
+a
+a
+a
+'''
+
+app.layout = html.Div(children=[
+        html.H1(children="Visualization Group 8", style={'textAlign':'center'}),
+        dcc.Markdown(markdown_text, style={'textAlign':'center'}),
+        html.Div([
+                
+                # Column 1
+                html.Div(children=[
+                        html.H3('Input'),
+                        html.H5('File-type', style={'backgroundColor': '#FFFFFF'}),
+                        html.Hr(),
                         dcc.Upload(
                             id='upload-data',
                             children=html.Div([
@@ -46,25 +53,32 @@ app.layout = html.Div([
                                 'lineHeight': '60px',
                                 'borderWidth': '1.5px',
                                 'borderStyle': 'dashed',
-                                'borderRadius': '5px',
+                                'borderRadius': '1px',
                                 'textAlign': 'center',
-                                'margin': '0px',
                                 'backgroundColor': '#FFFFFF'
                                 },
-                                multiple=False),
-                        html.Div(id='output-data-upload')
-                    ]
-                ),
-                
-            
-            # Start Column 3
-            html.Div(
-                    className= "six columns",
-                    children=[html.H3('Style')]
-                     )
-        ])
-])
+                                multiple=False)
+                                             
+                ], style={'width':'20%', 'display':'inline-block', 'margin':'50px', 'vertical-align':'top', 'backgroundColor':'#FFFFFF'}),
 
+
+                # Column 2
+                html.Div([
+                        html.H3('Visualization'),
+                        html.Hr(),
+                        html.Div(id='output-data-upload')
+                ], style={'width':'40%', 'display':'inline-block', 'vertical-align':'top', 'backgroundColor':'#FFFFFF'}),
+
+
+                # Column 3
+                html.Div([
+                        html.H3('Output'),
+                ], style={'width':'20%', 'display':'inline-block', 'margin':'50px', 'vertical-align':'top','backgroundColor':'#FFFFFF'})
+
+
+        ], className = 'row')
+])#, #style={'background-image':'url(https://image.freepik.com/free-vector/abstract-geometric-pattern-background_1319-242.jpg)'})
+  
 
 
 def parse_contents(contents, filename):
@@ -72,13 +86,12 @@ def parse_contents(contents, filename):
 
     decoded = base64.b64decode(content_string)
     try:
-        if 'txt' in filename:
-            array, df = sa.read_data(io.StringIO(decoded.decode('utf-8')))
+        array, df = sa.read_data(io.StringIO(decoded.decode('utf-8')))
     except Exception as e:
         print(e)
         return html.Div([
             'There was an error processing this file.'
-        ])
+        ], style={'backgroundColor': '#FFFFFF'})
 
     return html.Div([
         html.H5(filename),
@@ -94,7 +107,6 @@ def update_output(contents, filename):
         children = [
             parse_contents(contents, filename)]
         return children
-
 
 
 if __name__ == '__main__':
