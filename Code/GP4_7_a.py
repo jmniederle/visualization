1ff83a7fab04498a7921b9366c34b0e9d4527da5
jebkalisvaart <jebkalisvaart@users.noreply.github.com>
2018-12-08 15:11:26 +0100

    Update GP4_7_a.py

--- Code/GP4_7_a.py
+++ Code/GP4_7_a.py
@@ -4,12 +4,11 @@ Created on Fri Dec  7 11:12:25 2018
 
 @author: 20175878
 """
-
 import pandas as pd
 import networkx as nx
 import numpy as np
 
-final = pd.read_csv('C:/Users/20175878/Documents/GitHub/visualization/Dataset/profile_semantic_trafo_final.txt', delimiter='\s')
+final = pd.read_csv('profile_semantic_trafo_final.txt', delimiter='\s')
 
 dct = {}
 
@@ -23,9 +22,21 @@ for timestamp in dct:
     for tpl in dct[timestamp]:
       tpl[2] = np.log(tpl[2])
 
-G = nx.DiGraph()
-    
-for node in dct[48]:
-    G.add_edge(node[0], node[1], weight=node[2])
-    
-nx.draw(G, with_labels=True, node_size=100, node_color="skyblue", pos=nx.spring_layout(G))
+
+def draw_simple(timestamp):
+    G = nx.DiGraph()
+    for node in dct[timestamp]:
+        G.add_edge(node[0], node[1], weight=node[2])
+        
+    nx.draw(G, with_labels=True, node_size=100, node_color="skyblue", pos=nx.spring_layout(G, scale = 3))
+    #nx.draw(G, with_labels=True, node_size=100, node_color="skyblue", pos=nx.spring_layout(G, scale = 3), arrowstyle='fancy')
+def draw_weighted(timestamp):
+    G = nx.DiGraph()
+    for node in dct[timestamp]:
+        G.add_edge(node[0], node[1], weight=node[2])
+        
+#    nx.draw(G, with_labels=True, node_size=100, node_color="skyblue", pos=nx.spring_layout(G, scale = 3))
+    nx.draw(G, with_labels=True, node_size=100, node_color="skyblue", pos=nx.spring_layout(G, scale = 3), arrowstyle='fancy')
+
+draw_simple(48)
+draw_weighted(48)
