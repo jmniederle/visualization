2831e423a654d0dbe7e6abcb9e3023e7f463ba74
jebkalisvaart <jebkalisvaart@users.noreply.github.com>
2018-12-17 19:11:01 +0100

    zf

--- Code/GP4_7_a.py
+++ Code/GP4_7_a.py
@@ -50,29 +50,22 @@ draw_weighted(48)
 def get_nodes_edges_weights(timestamp):
     from_list = []
     for i in dct[timestamp]:
-        from_list.append((i[0],i[1]))
-    return(from_list)
-A = get_nodes_edges_weights(48)
+        from_list.append([(i[0],i[1])])
+    return(pd.DataFrame(from_list, columns= ['edges']))
 
+A = get_nodes_edges_weights(48)
+A
 def make_G(timestamp):
     G = nx.Graph()
     edges = get_nodes_edges_weights(timestamp)
-    G.add_edges_from(edges)
-    
+    G.add_edges_from(edges['edges'])
     return(G)
 
 G = make_G(48)
-pos=nx.get_node_attributes(G,'pos')
 
-dmin=1
-ncenter=0
-for n in pos:
-    x,y=pos[n]
-    d=(x-0.5)**2+(y-0.5)**2
-    if d<dmin:
-        ncenter=n
-        dmin=d
-       
+pos = nx.layout.spring_layout(G)
+
+#Create Edges
 edge_trace = go.Scatter(
     x=[],
     y=[],
@@ -85,7 +78,6 @@ for edge in G.edges():
     x1, y1 = G.node[edge[1]]['pos']
     edge_trace['x'] += tuple([x0, x1, None])
     edge_trace['y'] += tuple([y0, y1, None])
-#from storage_aggr import read_data
-#
-#array, df = read_data('profile_semantic_trafo_final.txt')
+
+
 
