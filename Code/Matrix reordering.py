4047143f96f3c43ae0615322cf6c80f3bce6e725
jebkalisvaart <jebkalisvaart@users.noreply.github.com>
2018-12-17 13:21:26 +0100

    Update Matrix reordering.py

--- Code/Matrix reordering.py
+++ Code/Matrix reordering.py
@@ -12,19 +12,31 @@ import numpy as np
 from sklearn.decomposition import PCA
 from sklearn.decomposition import SparsePCA
 import pandas as pd
+from collections import Counter
+
 
 #np.seterr(divide='ignore', invalid='ignore')
 
 
 array, df = read_data('profile_semantic_trafo_final.txt')
-df1 = pd.DataFrame(array[1])
-n_components = array.shape[1]
-pca = PCA(n_components = n_components)
-pca.fit(df1)
-pca.components_.shape
-components_df = pd.DataFrame(pca.components_)
-components_df
+array2 = array + 1
 
+# Maakt een lijst met de hoogste PCA component voor elke timestamp
+component_list = []
+for i in array2:
+    df1 = pd.DataFrame(i)
+    #n_components = 'mle'
+    pca = PCA(n_components = 1)
+    pca.fit(df1)
+    pca.components_.shape
+    components_df = pd.DataFrame(pca.components_)
+    component_list.append(components_df.max().idxmax())
+
+# Kijkt welke attribute het vaakst de hoogste CPA score heeft
+# Hier moet dus op gesort worden
+    
+data = Counter(component_list)
+data.most_common(1)
 
 
 
